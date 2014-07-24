b83dcca61526bb6017a6d3b38aa0b12107c35e5e
Chris Boehm <chrisboe@umich.edu>
2014-07-23 21:17:36 -0400

    First Version

---
+++ gen_sample.m
@@ -0,0 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Generate sample for the simple version of the model
+% Date: July 2014
+% Authors: Nitya and Chris
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Parameters
